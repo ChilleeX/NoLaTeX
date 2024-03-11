4b24e9b56ea9eda1cb4ef88a8947c990c14a3fb0
Philipp Bosler <philipp.bosler@gmail.com>
2024-03-11 16:50:46 -0300

    sorted out image loading and sorting out low contrast images

--- nolatex/preprocessing.py
+++ nolatex/preprocessing.py
@@ -2,21 +2,20 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import keras
-import keras_cv.layers
-from tensorflow.keras.utils import image_dataset_from_directory
-from keras.utils import load_imgfrom skimage.color import rgb2gray
+from keras_cv.layers import Resizing
+from keras_cv.layers import Grayscale
+from keras.utils import image_dataset_from_directory
+from keras.utils import load_img
 
-
-
-def image_preprocessing(image):
+def image_preprocessing(image, height, width):
     # 1 Convert RBG in Grayscale
-    to_grayscale = keras_cv.layers.Grayscale(output_channels=1)
+    to_grayscale = Grayscale(output_channels=1)
     gray_image = to_grayscale(image)
 
     # 2 Resize image to 254x254 with padding
-    resize_img = keras_cv.layers.Resizing(
-        254,
-        254,
+    resize_img = Resizing(
+        height,
+        width,
         interpolation="area",
         crop_to_aspect_ratio=False,
         pad_to_aspect_ratio=True,
@@ -24,3 +23,5 @@ def image_preprocessing(image):
     )
 
     resized_img = resize_img(gray_image)
+
+    return resized_img
